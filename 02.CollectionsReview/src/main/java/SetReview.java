ef216863b137e99978e2e284ec49c1eff25fcd6e
Bilal Ghani <bghani1995@gmail.com>
2022-08-29 13:15:53 -0500

    Set Review

---
+++ 02.CollectionsReview/src/main/java/SetReview.java
@@ -0,0 +1,21 @@
+public class SetReview {
+
+    public static void main(String[] args) {
+
+        //1. create a set
+        
+
+
+
+
+        // 2 add element
+
+
+
+    }
+    public static Character firstRepeatingChar(String str){
+
+        return null;
+    }
+
+}
