1efe74afe72e74d99ff13d81ceae731e7cf53d9f
Bilal Ghani <bghani1995@gmail.com>
2022-08-29 11:57:39 -0500

    Collections Review

--- 02.CollectionsReview/src/main/java/ArrayListReview.java
+++ 02.CollectionsReview/src/main/java/ArrayListReview.java
@@ -1,26 +1,58 @@
+import java.util.*;
+
 public class ArrayListReview {
 
     public static void main(String[] args) {
         // Create ArrayList and a class
+        List<Student> students = new ArrayList<>();
+
 
 
 // Add elements to ArrayList
+        students.add(new Student(1, "Jack"));
+        students.add(new Student(2, "Julia"));
+        students.add(new Student(3, "Mike"));
+        students.add(new Student(4, "Mary"));
 
 
         // Iteration on ArrayLists
         // 1. For Loop with get(index)
+        System.out.println("**************Printing with legacy for-loop***************");
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i));
+        }
 
         // 2. Iterator
         // Forward Iteration
+        System.out.println("**************Printing with iterator***************");
+        Iterator iter = students.listIterator();
+        while(iter.hasNext()){
+            System.out.println(iter.next());
+        }
 
 
         // Backwards Iteration
+        System.out.println("**************Printing Backwards with iterator***************");
+
+        while(((ListIterator<?>) iter).hasPrevious()){
+            System.out.println(((ListIterator<?>) iter).previous());
+        }
+
 
         // 3. for each loop
+        System.out.println("**************Printing with for each loop***************");
+        for (Student s: students) {
+            System.out.println(s);
+        }
 
 
         // 4. Lambda
+        System.out.println("**************Printing with lambda***************");
+        students.forEach(student -> System.out.println(student));
 
         // Sorting Elements in List
+        Collections.sort(students, new);
     }
+
+    class
 }
