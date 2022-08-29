2a51e50dcfa6cbdc7436fd46dc1aeb143621e2b1
Bilal Ghani <bghani1995@gmail.com>
2022-08-29 10:46:33 -0500

    Number Of People In Bus

--- 01.AlgorithmAnalysis/src/Assignment2_NumberOfPeopleInTheBus.java
+++ 01.AlgorithmAnalysis/src/Assignment2_NumberOfPeopleInTheBus.java
@@ -7,7 +7,8 @@ public class Assignment2_NumberOfPeopleInTheBus {
         ArrayList<int[]> busStops = new ArrayList<>();
         busStops.add(new int[] {10,0});//first stop
         busStops.add(new int[] {3,5});
-        busStops.add(new int[] {2,5});
+        busStops.add(new int[] {2,5});//last stop
+        System.out.println(countPassengers(busStops));
 
 
     }
@@ -17,6 +18,6 @@ public class Assignment2_NumberOfPeopleInTheBus {
         for (int i = 0; i < stops.size(); i++) {//Iterate each stop
             numberOfPeople = numberOfPeople+stops.get(i)[0] - stops.get(i)[1];// # of people who get on - # of people who get off bus
         }
-        return 0;
+        return numberOfPeople;
     }
 }
