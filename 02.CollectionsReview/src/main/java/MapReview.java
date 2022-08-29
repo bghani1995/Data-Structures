9553a32ea294f3b4823b38e41066394abdafa7cd
Bilal Ghani <bghani1995@gmail.com>
2022-08-29 14:22:04 -0500

    Map Review

--- 02.CollectionsReview/src/main/java/MapReview.java
+++ 02.CollectionsReview/src/main/java/MapReview.java
@@ -1,11 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapReview {
 
     public static void main(String[] args) {
+        // Create HashMap
+        Map<Integer, String> studentsMap = new HashMap<>();
+        studentsMap.put(1, "Jack");
+        studentsMap.put(2, "Julia");
+
+
 
 
     }
     public static Character findFirstNonRepeating(String str){
+        // create a map: this will cost me space complexity
+        Map<Character, Integer> map = new HashMap<>();
+        int count;
 
+        // count the frequency of chars
+        for(Character ch: str.toCharArray()){
+            if(map.containsKey(ch)) {
+                count=map.get(ch);
+                map.put(ch, count+1);
+            }
+            else map.put(ch, 1);
+        }
+
+        // start from the beginning and check if there is a char with frequency 1
+        for(Character ch: str.toCharArray()){
+            if(map.get(ch)==1) return ch;
+        }
         return null;
     }
 }
