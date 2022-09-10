4f4d18e84b5e4d7b0c0a6aaa5d4d927ef617c265
Bilal Ghani <bghani1995@gmail.com>
2022-09-10 01:49:26 -0500

    LinkedList

--- 03.LinkedLists/src/main/java/MySinglyLinkedList.java
+++ 03.LinkedLists/src/main/java/MySinglyLinkedList.java
@@ -8,5 +8,20 @@ public class MySinglyLinkedList {
         return head == null;
     }
 
+    void add(int data){
+        // Create a new Node object from data
+
+        Node node = new Node(data);
+        if(isEmpty()){// If the list is empty
+            head = tail = node;
+            size++;
+
+        }else{// If there are elements in the list
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+    }
+
 
 }
